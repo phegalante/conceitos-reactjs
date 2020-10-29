ae28ba4fdad819018ba6f015265fcfe6d175767e
phegalante <phegalante@gmail.com>
2020-10-29 18:03:57 -0300

    App

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
+	const [repositories, setRepositories] = useState([]);
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
+	useEffect(() => {
+		api.get("repositories").then(response => {
+			setRepositories(response.data);
+		});
+	}, []);
 
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+	async function handleAddRepository() {
+		const response = await api.post("/repositories", {
+			title: "front app React",
+			url: "Phelippe Novo",
+			techs: ["dasdfsddasd", "dasdas"],
+		});
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+		setRepositories([...repositories, response.data]);
+	}
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+	async function handleRemoveRepository(id) {
+		await api.delete(`/repositories/${id}`);
+
+		const repositoryIndex = repositories.findIndex(
+			repository => (repository.id = id)
+		);
+
+		const auxRepositories = [...repositories];
+		auxRepositories.splice(repositoryIndex, 1);
+		setRepositories(auxRepositories);
+	}
+
+	return (
+		<div>
+			<ul data-testid="repository-list">
+				{repositories.map(repository => {
+					return (
+						<li key={repository.id}>
+							{repository.title}
+							<button onClick={() => handleRemoveRepository(repository.id)}>
+								Remover
+							</button>
+						</li>
+					);
+				})}
+			</ul>
+
+			<button onClick={handleAddRepository}>Adicionar</button>
+		</div>
+	);
 }
 
 export default App;
